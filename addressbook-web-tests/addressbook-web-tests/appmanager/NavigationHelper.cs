3126e7d04bcd99fb11c356a61a8babdc7b7e5c80
Anvar Serojitdinov <myanvar@yahoo.com>
2019-08-05 23:01:53 -0400

    Ready for Homework

--- addressbook-web-tests/addressbook-web-tests/appmanager/NavigationHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/appmanager/NavigationHelper.cs
@@ -20,11 +20,20 @@ namespace WebAddressbookTests
         }
         public void GoToHomePage()
         {
-            driver.Navigate().GoToUrl(baseURL + "/addressbook/group.php");
+            if (driver.Url == baseURL + "/addresbook/")
+            {
+                return;
+            }
+            driver.Navigate().GoToUrl(baseURL + "/addressbook/");
         }
 
         public void GoToGroupsPage()
         {
+            if (driver.Url == baseURL + "/addressbook/group.php"
+                && IsElementPresent(By.Name("new")))
+            {
+                return;
+            }
             driver.FindElement(By.LinkText("groups")).Click();
         }
 
